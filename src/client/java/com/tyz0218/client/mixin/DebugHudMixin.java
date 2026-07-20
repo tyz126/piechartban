626c4d50b5264868b1399e1339f6c3f72236e7b3
tyz126 <tyz0218@yahoo.com>
2026-07-20 20:01:59 +0800

    idk

--- src/client/java/com/tyz0218/client/mixin/DebugHudMixin.java
+++ src/client/java/com/tyz0218/client/mixin/DebugHudMixin.java
@@ -1,15 +1,13 @@
 package com.tyz0218.client.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DebugHud.class)
-public class ExampleClientMixin {
+public class DebugHudMixin {
 	@Inject(at = @At("HEAD"), method = "shouldRenderTickCharts", cancellable = true)
 	public void shouldRenderTickCharts(CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(false);
--- src/main/java/com/tyz0218/PieChartBan.java
+++ src/main/java/com/tyz0218/PieChartBan.java
@@ -5,15 +5,21 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tyz0218.networking.HandshakeS2CPayload;
 
+import java.net.URI;
+
 public class PieChartBan implements ModInitializer {
-	public static final String MOD_ID = "anti-mcserverhost";
+	public static final String MOD_ID = "piechartban";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -27,14 +33,15 @@ public class PieChartBan implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		LOGGER.info("Hello Fabric world!");
+		//LOGGER.info("Hello Fabric world!");
+		String url = "https://modrinth.com/mod/pie-chart-ban";
 
 		PayloadTypeRegistry.playS2C().register(HandshakeS2CPayload.ID, HandshakeS2CPayload.CODEC);
 
 		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
 			if (!ServerPlayNetworking.canSend(handler, MOD_CHANNEL)) {
 				server.execute(() -> {
-					handler.disconnect(Text.literal("You must ban the pie chart to join this server!"));
+					handler.disconnect(Text.literal("You must download the Pie Chart Ban mod to join this server!\n").append(Text.literal(url).setStyle(Style.EMPTY.withUnderline(true)).formatted(Formatting.AQUA)));
 				});
 			}
 		});
