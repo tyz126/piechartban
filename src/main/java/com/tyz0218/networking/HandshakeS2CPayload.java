92870e2d9cdfeaf4fccaabb583bad8a180d05b29
tyz126 <tyz0218@yahoo.com>
2026-07-22 23:10:01 +0800

    idk

--- src/main/java/com/tyz0218/networking/HandshakeS2CPayload.java
+++ src/main/java/com/tyz0218/networking/HandshakeS2CPayload.java
@@ -10,7 +10,7 @@ import net.minecraft.util.Identifier;
 public record HandshakeS2CPayload(boolean idk) implements CustomPayload {
     public static Identifier HandshakeS2CPayloadID = PieChartBan.MOD_CHANNEL;
     public static final CustomPayload.Id<HandshakeS2CPayload> ID = new CustomPayload.Id<>(HandshakeS2CPayloadID);
-    public static final PacketCodec<RegistryByteBuf, HandshakeS2CPayload> CODEC = PacketCodec.tuple(PacketCodecs.BOOL, HandshakeS2CPayload::idk, HandshakeS2CPayload::new);
+    public static final PacketCodec<RegistryByteBuf, HandshakeS2CPayload> CODEC = PacketCodec.tuple(PacketCodecs.BOOLEAN, HandshakeS2CPayload::idk, HandshakeS2CPayload::new);
 
     @Override
     public Id<? extends CustomPayload> getId() {
